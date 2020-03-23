51f976ccd5eb6f7d6ae342d3311090e48be9454c
Quinn Krug <quinn.krug@gmail.com>
2020-03-23 15:18:36 -0600

    CSS module working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styles from './App.css';
+import classes from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
