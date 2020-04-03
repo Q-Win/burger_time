7bdc8b16bb036eeeefd5e3f70a45883e58d5435a
Quinn Krug <quinn.krug@gmail.com>
2020-04-03 08:29:06 -0600

    add Toolbar to Layout.js

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,13 +3,13 @@ import React from 'react';
 import classes from './Toolbar.css';
 
 const toolbar = (props) => (
-  <header>
+  <header className={classes.Toolbar}>
     <div>MENU</div>
-    <div>MENU</div>
+    <div>LOGO</div>
     <nav>
 
     </nav>
   </header>
 );
 
-export default toolbar
+export default toolbar;
