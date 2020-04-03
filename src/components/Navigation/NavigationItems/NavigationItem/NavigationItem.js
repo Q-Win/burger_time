3b4a39524fc82ec4048d2c57539e0c5e4cb4d410
Quinn Krug <quinn.krug@gmail.com>
2020-04-03 10:57:42 -0600

    fixed typos to get Navigation Items to load

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import classes from '.NavigationItem.css'
+import classes from './NavigationItem.css'
 
 const navigationItem = (props) => (
   <li className={classes.NavigationItem}>
     <a href={props.link}
-    class={props.active ? classes.active : null}>{props.children}</a>
+    className={props.active ? classes.active : null}>{props.children}</a>
   </li>
 );
 
